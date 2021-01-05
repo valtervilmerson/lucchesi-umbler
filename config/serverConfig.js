90a0d7c66fa5c9d47131e48715fd531b1a1e5201
valtervilmerson <valter.vilmerson@gmail.com>
2021-01-05 17:32:42 -0300

    Log de erro 500

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -40,6 +40,7 @@ app.use(function (req, res, next) {
 
 /* middleware que configura msgs de erro internos */
 app.use(function (err, req, res, next) {
+    console.log(err)
     res.status(500).render('errors/500', { error: err });
     next();
 });
